080ffcf2c1df8f86f1a1a0f27b3aeb629e552ed8
ebayraktaroglu <97638717+ebayraktaroglu@users.noreply.github.com>
2023-05-21 23:08:37 -0700

    I did the lab

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -15,6 +15,7 @@ window.addEventListener('DOMContentLoaded', init);
 
 // Starts the program, all function calls trace back here
 async function init() {
+	localStorage.clear();
   // initialize ServiceWorker
   initializeServiceWorker();
   // Get the recipes from localStorage
@@ -45,6 +46,17 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+	if("serviceWorker" in navigator){
+		window.addEventListener("load", (event) =>{
+			try{
+				navigator.serviceWorker.register("./sw.js");
+				console.log("service worker successfully registered");
+			}catch(err) {
+				console.log("service worker resitration failed");
+			}
+			
+		})
+	}
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
